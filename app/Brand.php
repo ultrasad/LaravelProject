2049f167c8a283863f6bf8bc1f7ff4a051cd9110
ultrasad <kissing-bear@hotmail.com>
2016-04-27 18:19:35 +0700

    Clear Larasearch
    Clear Larasearch

--- app/Brand.php
+++ app/Brand.php
@@ -3,6 +3,7 @@
 namespace App;
 
 use Illuminate\Database\Eloquent\Model;
+use Iverberk\Larasearch\Traits\SearchableTrait;
 
 class Brand extends Model
 {
--- app/Category.php
+++ app/Category.php
@@ -3,6 +3,7 @@
 namespace App;
 
 use Illuminate\Database\Eloquent\Model;
+use Iverberk\Larasearch\Traits\SearchableTrait;
 
 class Category extends Model
 {

4b3439d01e0d8480bca017bc782dacb7e3152388
ultrasad <kissing-bear@hotmail.com>
2016-06-16 15:42:48 +0700

    Update Setting Backend

--- resources/views/admin/setting.blade.php
+++ resources/views/admin/setting.blade.php
@@ -17,65 +17,56 @@
         <div class="panel panel-default">
           <div class="panel-body sm-p-t-20">
               <div class="form-group form-group-default required">
-                <label>หัวข้อข่าว</label>
-                <input type="text" name="title" class="form-control" placeholder="โปรโมชั่น" required />
+                <label>Meta Title</label>
+                <input type="text" name="title" class="form-control" placeholder="ชื่อเว็บ" required />
               </div>
-              <div class="form-group form-group-default required">
-                <label>URL SLUG (ภาษาอังกฤษเท่านั้น / สูงสุด 60 ตัวอักษร)</label>
-                <input type="text" name="url_slug" class="form-control" placeholder="ex: promotion-my-brand-my-name-date-year" required />
-              </div>
-              <div class="row">
-                <div class="col-md-6">
-                  <div class="form-group form-group-default input-group col-sm-12">
-                    <label>วันที่เริ่ม</label>
-                    <input type="text" name="start_date" class="form-control" placeholder="Pick a date" id="datepicker-component">
-                    <span class="input-group-addon"><i class="fa fa-calendar"></i></span>
-                  </div>
-                </div>
-                <div class="col-md-6">
-                  <div class="form-group form-group-default input-group col-sm-12">
-                    <label>วันสิ้นสุด</label>
-                    <input type="text" name="end_date" class="form-control" placeholder="Pick a date" id="datepicker-component2">
-                    <span class="input-group-addon"><i class="fa fa-calendar"></i></span>
-                  </div>
-                </div>
+              <div class="form-group form-group-default form-group-area required">
+                <label>Meta Deacription</label>
+                <textarea class="form-control" name="site_deacription" rows="3" required></textarea>
               </div>
               <div class="form-group form-group-default input-group">
-                <label>รูปภาพหลัก</label>
+                <label>Meta Image</label>
                 <input type="text" class="form-control" />
                 <span class="input-group-addon btn-file">
-                    <input type="file" name="image" class="form-control form-control-image" id="image" placeholder="รูปภาพ" readonly />
+                    <input type="file" name="site_image" class="form-control form-control-image" id="site_image" placeholder="รูปภาพ" readonly />
                     <i class="fa fa-picture-o icon-picture"></i>
                 </span>
               </div>
-
-              <!-- END PANEL -->
               <div class="form-group form-group-default required">
-                <label>Keyword (สูงสุด 20 คำ)</label>
-                <input class="tagsinput custom-tag-input validate" id="tag_list" name="tag_list" type="text" value="" />
+                <label>Facebook</label>
+                <input type="text" name="facebook_url" class="form-control" placeholder="URL Facebook" />
+              </div>
+              <div class="form-group form-group-default required">
+                <label>Twitter</label>
+                <input type="text" name="twitter_url" class="form-control" placeholder="URL Twitter" />
+              </div>
+              <div class="form-group form-group-default required">
+                <label>Youtube</label>
+                <input type="text" name="youtube_url" class="form-control" placeholder="URL Youtube" />
               </div>
               <div class="form-group form-group-default form-group-area required">
-                <label>รายละเอียดแบบย่อ</label>
-                <textarea class="form-control" name="brief" rows="3" required></textarea>
+                <label>Email Contact (Separate with comma.)</label>
+                <textarea class="form-control" name="site_deacription" rows="3" required></textarea>
               </div>
+              <!-- END PANEL -->
+          </div>
+        </div>
+        <div class="col-md-12">
+          <div class="row">
+            <button class="btn btn-success" type="submit" id="submit_event">Submit</button>
+            <button class="btn btn-default"><i class="pg-close"></i> Clear</button>
           </div>
         </div>
         <!-- END PANEL -->
       </div>
-      <div class="col-md-4">
+      <!--<div class="col-md-6">
         <div class="row">
           <button class="btn btn-success" type="submit" id="submit_event">Submit</button>
           <button class="btn btn-default"><i class="pg-close"></i> Clear</button>
         </div>
-      </div>
+      </div>-->
     </div>
   </div>
   </form>
   <!-- END CONTAINER FLUID -->
-
-  <div class="event_branch_row" style="display: none;">
-    <div class="checkbox check-warning branch_row">
-      <input type="checkbox" id="branch_x" value="" class="branch" name="branch[]" checked="checked"><label for="branch_x">สาขา</label>
-    </div>
-  </div>
 @stop

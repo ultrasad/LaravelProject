c75ae12881e128d855aac973f10d6bf363a0fd8e
ultrasad <kissing-bear@hotmail.com>
2016-07-07 17:57:38 +0700

    Update Footer Analytics and Truehit

--- resources/views/includes/foot.blade.php
+++ resources/views/includes/foot.blade.php
@@ -18,3 +18,14 @@
 <!--<script type="text/javascript" src="{{ elixir('js/scripts.js') }}"></script>-->
 <script type="text/javascript" src="{{ URL::asset('assets/js/scripts.js?v=1.0.0') }}"></script>
 <!--<script src="https://maps.googleapis.com/maps/api/js?callback=initialize&libraries=places" async defer></script>-->
+
+<!--- GoogleAnalytics --->
+<script type="text/javascript">
+  (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
+  (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
+  m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
+  })(window,document,'script','https://www.google-analytics.com/analytics.js','ga');
+
+  ga('create', 'UA-7620437-1', 'auto');
+  ga('send', 'pageview');
+</script>
--- resources/views/includes/footer.blade.php
+++ resources/views/includes/footer.blade.php
@@ -12,6 +12,13 @@
         <a target="_blank" href="http://twitter.com/welovepro" title="Follow us on Twitter" class=""><i class="fa fa-twitter fa-lg" aria-hidden="true"></i></a>
         <a target="_blank" href="#" title="Follow us on Youtube" class=""><i class="fa fa-youtube fa-lg" aria-hidden="true"></i></a>
         <a href="/contact" title="Contact Us" class=""><i class="fa fa-envelope fa-lg" aria-hidden="true"></i></a>
+		<!--BEGIN WEB STAT CODE-->
+		<script type="text/javascript" src="http://hits.truehits.in.th/data/t0029483.js"></script>
+		<noscript>
+		<a target="_blank" href="http://truehits.net/stat.php?id=t0029483"><img src="http://hits.truehits.in.th/noscript.php?id=t0029483" alt="Thailand Web Stat" border="0" width="14" height="17" /></a>
+		<a target="_blank" href="http://truehits.net/">Truehits.net</a>
+		</noscript>
+		<!-- END WEBSTAT CODE -->
       </div>
     </p>
     <div class="clearfix"></div>
